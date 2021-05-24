3f68280d5d142f844d0de45cb66d44aa245e0b5f
Desno365 <desno365@gmail.com>
2021-05-24 18:23:35 +0200

    Specify parameters types in Ad and Slot

--- ad.py
+++ ad.py
@@ -2,7 +2,7 @@ class Ad:
     # ad_id = identifier for the ad.
     # ad_quality = probability given the ad that the user clicks it.
     # ad_value = bid for the ad.
-    def __init__(self, ad_id, ad_quality, ad_value):
+    def __init__(self, ad_id: int, ad_quality: float, ad_value: float):
         self.ad_id = ad_id
         self.ad_quality = ad_quality
         self.ad_value = ad_value
--- slot.py
+++ slot.py
@@ -1,18 +1,21 @@
+from ad import Ad
+
+
 class Slot:
     # slot_id = identifier for the slot.
     # slot_prominence = probability that the user observes the slot.
     # price_per_click = price that advertiser must pay of ads gets clicked.
-    def __init__(self, slot_id, slot_prominence):
+    def __init__(self, slot_id: int, slot_prominence: float):
         self.slot_id = slot_id
         self.slot_prominence = slot_prominence
         self.assigned_ad = None
         self.price_per_click = None
 
-    def update_assigned_ad(self, assigned_ad):
+    def update_assigned_ad(self, assigned_ad: Ad):
         self.assigned_ad = assigned_ad
         self.price_per_click = 0.0
 
-    def update_price_per_click(self, price_per_click):
+    def update_price_per_click(self, price_per_click: float):
         self.price_per_click = price_per_click
 
     def __str__(self):
