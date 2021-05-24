8c6267a4f84201ebe4126add7ed0cd6110154f9a
Desno365 <desno365@gmail.com>
2021-05-24 18:40:30 +0200

    Optimization: execute deep copy of slate only one time instead of one time for every slot

--- vcg_auction.py
+++ vcg_auction.py
@@ -17,6 +17,7 @@ class VCGAuction:
         print(f'Initializing auction. Received ads with length {len(available_ads)}, slate with length {len(slate)}')
 
         # Check that we have the minimum number of available ads to cover the slate.
+        # Note: uses ">" and not ">=" because for calculating the price of an ad we need the existence of another ad that could substitute it.
         assert len(available_ads) > self.num_of_slots
 
         # Check that the slots array has its items sorted by prominence.
@@ -32,11 +33,12 @@ class VCGAuction:
             self.slate[i].update_assigned_ad(assigned_ad=best_ads[i])
 
         # Compute the price for every ad assigned to a slot. This basically implements what is written here https://i.imgur.com/0kWZa7E.jpg
+        slate_deep_copy = copy.deepcopy(self.slate)  # "compute_x_a" will modify the assignments of the slate. So to avoid modifying the original slate we make a deep copy.
         for slot in self.slate:
             ad_id = slot.assigned_ad.ad_id
             slot_prominence_a = slot.slot_prominence
             quality_a = slot.assigned_ad.ad_quality
-            x_a = VCGAuction.compute_x_a(ads=self.available_ads, slate=self.slate, a_id=ad_id)
+            x_a = VCGAuction.compute_x_a(ads=self.available_ads, slate=slate_deep_copy, a_id=ad_id)
             y_a = VCGAuction.compute_y_a(allocated_slate=self.slate, a_id=ad_id)
 
             p_a = (1 / (slot_prominence_a * quality_a)) * (x_a - y_a)
@@ -60,21 +62,19 @@ class VCGAuction:
     # a_id = id of the ad "a" that we want to use to compute x_a.
     # Returns x_a that is basically the total gain that other ads produce to the publisher if ad "a" does not exist.
     # Formally x_a is: using the best assignment without ad a: sum on every ad a' different than a: slot_prominence of ad a' in assignment * quality of ad a' * value of ad a'
+    # WARNING: this method modifies the slate by assigning the best ads. If this is not wanted make a deep copy of the slate!
     def compute_x_a(ads: List[Ad], slate: List[Slot], a_id: int):
         available_ads_without_a = [ad1 for ad1 in ads if ad1.ad_id != a_id]
         return VCGAuction.get_total_gain_of_best_allocation(ads=available_ads_without_a, slate=slate)
 
     @staticmethod
+    # WARNING: this method modifies the slate by assigning the best ads. If this is not wanted make a deep copy of the slate!
     def get_total_gain_of_best_allocation(ads: List[Ad], slate: List[Slot]):
-        # This method will modify the assignments of the slate.
-        # So to avoid modifying the original slate we make a deep copy.
-        slate_copy = copy.deepcopy(slate)
-
-        num_of_slots = len(slate_copy)
+        num_of_slots = len(slate)
         best_ads = VCGAuction.get_best_ads(ads=ads)
         for i in range(num_of_slots):
-            slate_copy[i].update_assigned_ad(assigned_ad=best_ads[i])
-        return VCGAuction.get_total_gain_of_allocation(allocated_slate=slate_copy, except_ad_id=None)
+            slate[i].update_assigned_ad(assigned_ad=best_ads[i])
+        return VCGAuction.get_total_gain_of_allocation(allocated_slate=slate, except_ad_id=None)
 
     @staticmethod
     def get_best_ads(ads: List[Ad]):
