8b8a7f5b7199f159bad0312b0e1bae33fbe89a2f
Desno365 <desno365@gmail.com>
2021-09-04 11:58:02 +0200

    Add StochasticStationaryAdvertiser

---
+++ src/stochastic_stationary_advertiser.py
@@ -0,0 +1,22 @@
+import random
+
+from src.advertiser import Advertiser
+from src.bids_enum import BidsEnum
+
+"""
+An advertiser that has its bids set randomly.
+"""
+
+
+class StochasticStationaryAdvertiser(Advertiser):
+
+    def __init__(self, quality=None, value=0.5):
+        super().__init__(quality, value)
+        self.change_bids()
+
+    def participate_auction(self):
+        return super().participate_auction()
+
+    def change_bids(self):
+        self.bids = [random.choice(list(BidsEnum)) for _ in range(5)]
+        self.ad.setbids(self.bids)
