1b4445286935b268a8ee7341c63f014e4c24f38c
Desno365 <desno365@gmail.com>
2021-09-08 16:40:39 +0200

    Finish step1 (or almost finished)

--- src/step1.py
+++ src/step1.py
@@ -1,6 +1,7 @@
 import random
 from typing import List
 
+import numpy as np
 from matplotlib import pyplot as plt
 
 from src import ad, slot, constants, network
@@ -13,9 +14,9 @@ def get_random_bids() -> List[BidsEnum]:
     return [random.choice(list(BidsEnum)) for _ in range(5)]
 
 
-BEST_PERFORMANCE = 10.0  # TODO calculate
-print('ok')
-network_instance = network.Network(50, False)
+# ################ Prepare experiment. ################ #
+
+network_instance = network.Network(100, False)
 
 print("Ads:")
 advertisements = [ad.Ad(0, [0.5, 0.1, 0.1, 0.1, 0.1], 1, get_random_bids()),
@@ -31,23 +32,38 @@ for current_category in range(constants.CATEGORIES):
     slates.append(slate)
 Utils.print_array(slates)
 
+
+# ################ Run experiment. ################ #
+
 number_of_activated_nodes_per_experiment = []
 list_iterations = []
-for i in range(1, 100, 1):
-    iterations = i
+log_range = np.logspace(0, 4, num=100, dtype='int')  # Starting from 10^0 to 10^4.
+log_range = list(dict.fromkeys(log_range))
+print("Using these values as possible iterations:")
+print(log_range)
+
+for iterations in log_range:
+    print("Running with iterations " + str(iterations))
     list_iterations.append(iterations)
     r = AdPlacementSimulator.simulate_ad_placement(network=network_instance, ads=advertisements, slates=slates, iterations=iterations)
     number_of_activated_nodes_per_experiment.append(r[0]['activatedNodes'])
+best_perfomance = number_of_activated_nodes_per_experiment[-1]
+
+
+# ################ Plot result. ################ #
 
 plt.figure(0)
 plt.xlabel("Iterations")
 plt.ylabel("Activated nodes")
-plt.plot(number_of_activated_nodes_per_experiment, 'r')
-#plt.ylim(ymin=0)
+plt.plot(log_range, number_of_activated_nodes_per_experiment, 'r')
+plt.plot([best_perfomance for i in range(len(number_of_activated_nodes_per_experiment))], 'b--')
+plt.ylim(ymin=0)
 plt.show()
 
-plt.plot(list_iterations, number_of_activated_nodes_per_experiment)
+plt.figure(1)
+plt.xlabel("Iterations")
+plt.ylabel("Activated nodes")
+plt.plot(log_range, number_of_activated_nodes_per_experiment, 'r')
+plt.plot([best_perfomance for i in range(len(number_of_activated_nodes_per_experiment))], 'b--')
 plt.show()
-
-
 
