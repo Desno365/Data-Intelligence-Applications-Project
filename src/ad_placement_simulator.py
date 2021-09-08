37940d87540e803d72172b119974ed20454c6120
Desno365 <desno365@gmail.com>
2021-09-08 14:47:33 +0200

    Implement auction part in AdPlacementSimulator

--- src/ad_placement_simulator.py
+++ src/ad_placement_simulator.py
@@ -1,8 +1,11 @@
 from typing import List
 
 from src.ad import Ad
+from src.auction.auction_ad import AuctionAd
+from src.auction.vcg_auction import VCGAuction
 from src.constants import CATEGORIES
 from src.slot import Slot
+from src.utils import Utils
 
 
 class AdPlacementSimulator:
@@ -10,20 +13,30 @@ class AdPlacementSimulator:
     @staticmethod
     # ads = the list of ads available, every ad comes from an advertiser.
     # slates = the list of slates, one slate per category (a slate is a list of slots).
-    def simulate_ad_placement(ads: List[Ad], slates: List[List[Slot]], iterations: int) -> None:
+    def simulate_ad_placement(ads: List[Ad], slates: List[List[Slot]], iterations: int):
 
         # The auction must be simulated for each category.
-        for x in range(CATEGORIES):
-            # Per ogni categoria simulo la auction.
-            # Auction ha bisogno di bid e quality di tutti gli advertiser per quella categoria.
-            # Auction ha bisogno dello slate per quella categoria.
-            # Attenzione che gli slot prominenace sono uguali per tutti gli slate.
-            # La auction poi restituisce lo slate dato in input ma con pubblicità assegnate ad ogni slot e il prezzo per ogni slot.
+        for current_category in range(CATEGORIES):
+            # For each category we need to perform an auction.
+            # The auction needs the bid and quality of all the ads for that category.
+            # The auction also need the slate of the category.
+            # Note: the slot prominence is equal for all slate, the only thing changing is the slot object itself.
+            # The auction then will return the slate itself but with an ad and price assigned to every slot.
+            auction_ads_for_the_category = []
+            for ad in ads:
+                new_auction_ad = AuctionAd(ad_id=ad.ad_id, ad_quality=ad.ad_quality[current_category], ad_bid=ad.ad_value)
+                auction_ads_for_the_category.append(new_auction_ad)
 
-            # Adesso abbiamo uno slate completo per la categoria.
-            # Salviamo lo slate in una lista, per avere poi una lista completa con tutti gli slate per tutte le categorie.
-            print("")
+            slate_of_the_category = slates[current_category]
+            vcg_auction = VCGAuction(available_ads=auction_ads_for_the_category, slate=slate_of_the_category)
+            slate_with_assigned_ads = vcg_auction.perform_auction()
+            Utils.print_array(slate_with_assigned_ads)
 
+            # Overwrite information of the slate with the new assigned slate.
+            # Now we have ready the slate of the current category with all the information needed.
+            slates[current_category] = slate_with_assigned_ads
+
+        # TODO: for monte carlo use the "slates" variable
         # Monte Carlo:
         # Input: lista di slate con pubblicità assegnate e prezzi.
         # Ouput: dictionary che per ogni ad_id (univoco per advertiser) specifica numero medio di seed e nodi attivati.
