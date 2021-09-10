dbecce9590072d2ee6525f1fa00e3428ffceb9ff
Eugenio <eugen951@gmail.com>
2021-09-10 09:54:30 +0200

    better comments

--- src/step4.py
+++ src/step4.py
@@ -1,17 +1,22 @@
 # bandit learner for click probabilities in publisher (ad.ad_quality * slot.slot_prominence)
-# arm = [click probability for ad in ads]
 # len(ads) = len(advertisers)
 # len(qualities) = len(ads) * constants.CATEGORIES
+
 # does the publisher know the slot prominence values and ad placements so that we estimate only qualities?
 # ad quality is constant, but slot prominence depends on ad placement and advertiser bids so it could in the worst case
 # be different every time
+
 # one bandit with many arms
 # one bandit with arm = [[quality for _ in range(constants.CATEGORIES)] for _ in range(len(ads)]
+# number of arms is number of discrete values**(len(ads)*constants.CATEGORIES)
 # or
 # many bandits with fewer arms (one bandit for every ad)
 # len(ads) = len(bandits) with arm = [quality for _ in range(constants.CATEGORIES)]
-# individual click probability values are i in range(0, 1.1, 0.1), 11 values total
-# number of arms is 11**len(ads)
+# number of arms is number of discrete values**(constants.CATEGORIES)
+
+# individual click probability values are i in range(0, 1.1, 0.1), 11 values total (could be too many)
+# or i in range(0, 1.1, 0.25), only 5 values
+
 # pass click probability to advertiser
 # advertiser passes click probability to monte carlo
 # if click probability = None then
