ee2bb6520a1379a6cc0f81f61af808ad97446855
Desno365 <desno365@gmail.com>
2021-09-13 18:09:15 +0200

    Add RandomLearner in tests for bandits

--- src/tests/bandit_algorithms/experiments/ts_vs_ucb1_vs_greedy_experiment.py
+++ src/tests/bandit_algorithms/experiments/ts_vs_ucb1_vs_greedy_experiment.py
@@ -1,3 +1,4 @@
+import random
 from unittest import TestCase
 
 import numpy as np
@@ -7,6 +8,7 @@ from src.bandit_algorithms.thompson_sampling_learner import ThompsonSamplingLear
 from src.bandit_algorithms.ucb1_learner import UCB1Learner
 from src.tests.bandit_algorithms.environments.bandit_test_environment import BanditTestEnvironment
 from src.tests.bandit_algorithms.greedy_learner import GreedyLearner
+from src.tests.bandit_algorithms.random_learner import RandomLearner
 
 
 class TestThompsonSamplingVsUCB1VsGreedyExperiment(TestCase):
@@ -21,12 +23,13 @@ class TestThompsonSamplingVsUCB1VsGreedyExperiment(TestCase):
 
         # Horizon.
         # Note: we needed to increase the horizon to see effect of UCB1.
-        time_horizon = 2000
+        time_horizon = 200
 
         # Experiment variables.
-        n_experiments = 200
+        n_experiments = 100
         ts_rewards_per_experiment = []
         gr_rewards_per_experiment = []
+        random_rewards_per_experiment = []
         ucb1_rewards_per_experiment = []
 
         # ################ Run experiment. ################ #
@@ -36,6 +39,7 @@ class TestThompsonSamplingVsUCB1VsGreedyExperiment(TestCase):
             env = BanditTestEnvironment(n_arms=n_arms, probabilities=p)
             ts_learner = ThompsonSamplingLearner(n_arms=n_arms)
             gr_learner = GreedyLearner(n_arms=n_arms)
+            random_learner = RandomLearner(n_arms=n_arms)
             ucb1_learner = UCB1Learner(n_arms=n_arms)
 
             for i in range(time_horizon):
@@ -49,6 +53,11 @@ class TestThompsonSamplingVsUCB1VsGreedyExperiment(TestCase):
                 reward = env.round(pulled_arm)
                 gr_learner.update(pulled_arm, reward)
 
+                # Simulate interaction between environment and random learner.
+                pulled_arm = random_learner.pull_arm()
+                reward = env.round(pulled_arm)
+                random_learner.update(pulled_arm, reward)
+
                 # Simulate interaction between environment and UCB1 learner.
                 pulled_arm = ucb1_learner.pull_arm()
                 reward = env.round(pulled_arm)
@@ -57,6 +66,7 @@ class TestThompsonSamplingVsUCB1VsGreedyExperiment(TestCase):
             # Store rewards of the experiment.
             ts_rewards_per_experiment.append(ts_learner.collected_rewards)
             gr_rewards_per_experiment.append(gr_learner.collected_rewards)
+            random_rewards_per_experiment.append(random_learner.collected_rewards)
             ucb1_rewards_per_experiment.append(ucb1_learner.collected_rewards)
 
         # ################ Preprocess result. ################ #
@@ -64,6 +74,7 @@ class TestThompsonSamplingVsUCB1VsGreedyExperiment(TestCase):
         # Mean over the regret of all n_experiments experiments, for TS, Greedy and UCB1.
         ts_mean_regrets = np.mean(opt - ts_rewards_per_experiment, axis=0)
         gr_mean_regrets = np.mean(opt - gr_rewards_per_experiment, axis=0)
+        random_mean_regrets = np.mean(opt - random_rewards_per_experiment, axis=0)
         ucb1_mean_regrets = np.mean(opt - ucb1_rewards_per_experiment, axis=0)
 
         # ################ Plot result. ################ #
@@ -73,8 +84,9 @@ class TestThompsonSamplingVsUCB1VsGreedyExperiment(TestCase):
         plt.ylabel("Regret")
         plt.plot(np.cumsum(ts_mean_regrets), 'r')
         plt.plot(np.cumsum(gr_mean_regrets), 'g')
+        plt.plot(np.cumsum(random_mean_regrets), 'c')
         plt.plot(np.cumsum(ucb1_mean_regrets), 'b')
-        plt.legend(["TS", "Greedy", "UCB1"])
+        plt.legend(["TS", "Greedy", "Random", "UCB1"])
         plt.show()
 
         # We can see that:
---
+++ src/tests/bandit_algorithms/random_learner.py
@@ -0,0 +1,27 @@
+import random
+
+import numpy as np
+
+from src.bandit_algorithms.bandit_learner import BanditLearner
+
+
+class RandomLearner(BanditLearner):
+    # n_arms = number of arms the learner can pull.
+    def __init__(self, n_arms: int):
+        super().__init__(n_arms)
+
+    # Select which arm to pull randomly.
+    def pull_arm(self) -> int:
+        if self.t < self.n_arms:
+            return self.t  # Guarantees that each arm is pulled once.
+
+        return random.randint(0, self.n_arms-1)  # randint(a, b) return a random integer N such that a <= N <= b.
+
+    # pulled_arm = arm pulled.
+    # reward = reward of arm pulled.
+    def update(self, pulled_arm: int, reward: float) -> None:
+        # Increment round.
+        self.t += 1
+
+        # Updates rewards lists.
+        self.update_observations(pulled_arm, reward)
