07de8c711af3b2727f8997d877be323f3ccbb269
Desno365 <desno365@gmail.com>
2021-09-15 17:41:44 +0200

    Change variables of step 3 to produce draft of final graphs

--- src/step3.py
+++ src/step3.py
@@ -3,22 +3,26 @@ from src.advertiser.greedy_learning_advertiser import GreedyLearningAdvertiser
 from src.advertiser.stochastic_stationary_advertiser import StochasticStationaryAdvertiser
 from src.utils import Utils
 
-NUMBER_OF_STOCHASTIC_ADVERTISERS = constants.SLATE_DIMENSION + 10
+NUMBER_OF_STOCHASTIC_ADVERTISERS = constants.SLATE_DIMENSION + 5
 
 network_instance = network.Network(100, False)
 
 print("Slates:")
 slates = constants.slates
-Utils.print_array(slates)
+for slate in slates:
+    Utils.print_array(slate)
 
-print("Ads:")
 advertisers = [StochasticStationaryAdvertiser(ad_real_qualities=None) for _ in range(NUMBER_OF_STOCHASTIC_ADVERTISERS)]
-for advertiser in advertisers:
-    print('ad id', advertiser.id, 'bids', advertiser.ad.bids)
-greedy_learner = GreedyLearningAdvertiser(ad_real_qualities=[1 for _ in range(constants.CATEGORIES)], ad_value=1, network=network_instance)
+greedy_learner = GreedyLearningAdvertiser(ad_real_qualities=[0.8 for _ in range(constants.CATEGORIES)], ad_value=1, network=network_instance)
 greedy_learner.set_rival_ads(rival_ads=[advertiser.ad for advertiser in advertisers])
 greedy_learner.set_slates(slates=slates)
 advertisers.append(greedy_learner)
-greedy_ad = greedy_learner.participate_auction()
 
+print("Ads:")
+for advertiser in advertisers:
+    print('ad id', advertiser.id, 'bids', advertiser.ad.bids)
+for advertiser in advertisers:
+    print('ad id', advertiser.id, 'qualities', advertiser.ad.estimated_qualities)
+
+greedy_ad = greedy_learner.participate_auction()
 greedy_learner.plot_gain_history_in_single_day()
