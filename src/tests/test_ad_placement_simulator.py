d9796cf1bfce794746838053f2a29a49c0b23a03
Marchingegno <matteo.marchisciana@mail.polimi.it>
2021-09-20 15:32:34 +0200

    Changes by Desno365

--- src/tests/test_ad_placement_simulator.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-from src.auction.vcg_auction import VCGAuction
-from src.utils import Utils
-
-
-class TestAdPlacementSimulator(TestCase):
-    def test_perform_simulation(self):
-        print("cc")
